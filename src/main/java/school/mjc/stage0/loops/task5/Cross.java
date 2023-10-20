add777509685e54d9fb46d4cfcdb022440c9117a
Fanny Dorthey <fannydor@ev-k6.kzn.21-school.ru>
2023-10-20 03:19:41 +0300

    asffwfwqe

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -2,6 +2,18 @@ package school.mjc.stage0.loops.task5;
 
 public class Cross {
     public void printCross(int sideLength) {
-
+        int middle = sideLength / 2;
+        for (int i = 1; i <= sideLength; i++) {
+            for(int j = 1; j <= sideLength; j++) {
+                if (j == middle +1) {
+                    System.out.print("8");
+                }else if(i == middle + 1) {
+                    System.out.print("8");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -3,5 +3,36 @@ package school.mjc.stage0.loops.task5;
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
 
+
+        for (int i = 0; i < height; i++) {
+            // Print leading spaces
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+
+            // Print '8's
+            for (int j = 0; j < 2 * (height - i) - 1; j++) {
+                System.out.print("8");
+            }
+
+            // Move to the next line
+            System.out.println();
+        }
+
+        for (int i = height - 2; i >= 0; i--) {
+            // Print leading spaces
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+
+            // Print '8's
+            for (int j = 0; j < 2 * (height - i) - 1; j++) {
+                System.out.print("8");
+            }
+
+            // Move to the next line
+            System.out.println();
+        }
+
     }
 }
